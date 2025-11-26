f1b5b84b36950813d9b7b93d5f79079e62d7c7cf
Kyle Walker <Kyle@Walker2003.com>
2025-11-26 15:36:24 -0500

    testing different ints

--- test_romanNumeral.py
+++ test_romanNumeral.py
@@ -4,3 +4,9 @@ from romanNumeral import RomanNumeral
 class TestRomanNumeral(unittest.TestCase):
     def setUp(self):
         self.r = RomanNumeral()
+        
+    def test_toRoman(self):
+        self.assertEqual(self.r.toRoman(1), 'I')
+        self.assertEqual(self.r.toRoman(12), 'XII')
+        self.assertEqual(self.r.toRoman(149), 'CXLIX')
+        self.assertEqual(self.r.toRoman(2841), 'MMDCCCXLI')
